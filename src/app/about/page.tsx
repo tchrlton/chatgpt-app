765c4888c24920c9641d014fb9a69e139181f153
tcha295 <tcha295@wgu.edu>
2024-09-28 02:21:58 -0700

    Layout

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <main>
+      <h1 className="text-4xl font-bold">About Us</h1>
+      <div className="mt-5">We specialize in using and managing your AI chats using ChatGPT.</div>
+    </main>
+  )
+}
